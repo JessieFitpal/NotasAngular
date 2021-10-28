2ab1c9c0433e9a57bffb6a172ae4da78558c7243
JessieCardenas <ycardenas@fitpal.co>
2021-10-28 12:42:56 -0500

    Jessie

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDBCJI7UlWnv325oHvMur7iSLRQeFHwrLU",
+    authDomain: "notasangular-732b2.firebaseapp.com",
+    projectId: "notasangular-732b2",
+    storageBucket: "notasangular-732b2.appspot.com",
+    messagingSenderId: "445421210116",
+    appId: "1:445421210116:web:d0b7507fd184730f07c351"
+  }
 };
